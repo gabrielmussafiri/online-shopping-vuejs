c9432b6aedcc33fe6bce0b258fe907dc8c891347
gabriel <musgabriel2@gmail.com>
2023-08-29 01:14:09 +0200

    Create  catalog view

---
+++ src/views/Catalog.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>
+        Catalogue
+    </h1>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name:'CatalogView'
+})
+
+
+</script>
